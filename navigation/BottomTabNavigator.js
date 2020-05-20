0d5ccd05e4b0e6308190233a1cb595394aac52bb
jsc <jsanchezcriado@gmail.com>
2020-05-20 16:52:33 +0200

    changed NavigationContainer LinkingConfiguration and the navigation for the BottomTabNavigator

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,12 +1,12 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import * as React from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as React from "react";
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import LinksScreen from "../screens/LinksScreen";
 
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const INITIAL_ROUTE_NAME = "Decks";
 
 export default function BottomTabNavigator({ navigation, route }) {
   // Set the header title on the parent stack navigator depending on the
@@ -17,19 +17,23 @@ export default function BottomTabNavigator({ navigation, route }) {
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
       <BottomTab.Screen
-        name="Home"
+        name="Decks"
         component={HomeScreen}
         options={{
-          title: 'Get Started',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+          title: "Decks",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-list-box" />
+          ),
         }}
       />
       <BottomTab.Screen
-        name="Links"
+        name="AddDeck"
         component={LinksScreen}
         options={{
-          title: 'Resources',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
+          title: "Add Deck",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-add-circle" />
+          ),
         }}
       />
     </BottomTab.Navigator>
@@ -37,12 +41,13 @@ export default function BottomTabNavigator({ navigation, route }) {
 }
 
 function getHeaderTitle(route) {
-  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+  const routeName =
+    route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
   switch (routeName) {
-    case 'Home':
-      return 'How to get started';
-    case 'Links':
-      return 'Links to learn more';
+    case "Decks":
+      return "Deck list";
+    case "Add Deck":
+      return "Add a deck to your decks";
   }
 }
