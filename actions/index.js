829a0c9f681d11c85b786c03b46c5a2435a0ab1f
jsc <jsanchezcriado@gmail.com>
2020-05-20 19:01:23 +0200

    added actions and reducers

---
+++ actions/index.js
@@ -0,0 +1,42 @@
+export const RECEIVE_DECKS = "RECEIVE_DECKS";
+export const ADD_DECK = "ADD_DECK";
+export const ADD_CARD_TO_DECK = "ADD_CARD_TO_DECK";
+export const REMOVE_DECK = "REMOVE_DECK";
+export const REMOVE_DECK_CARD = "REMOVE_DECK_CARD";
+
+export const receiveDecks = (decks) => {
+  return {
+    type: RECEIVE_DECKS,
+    decks,
+  };
+};
+
+export const addDeck = (title) => {
+  return {
+    type: ADD_DECK,
+    title,
+  };
+};
+
+export const addCardToDeck = (title, card) => {
+  return {
+    type: ADD_CARD_TO_DECK,
+    title,
+    card,
+  };
+};
+
+export const removeDeck = (title) => {
+  return {
+    type: REMOVE_DECK,
+    title,
+  };
+};
+
+export const removeDeckCard = (title, card) => {
+  return {
+    type: REMOVE_DECK_CARD,
+    title,
+    card,
+  };
+};
---
+++ reducers/index.js
@@ -0,0 +1,53 @@
+import {
+  RECEIVE_DECKS,
+  ADD_DECK,
+  ADD_CARD_TO_DECK,
+  REMOVE_DECK,
+  REMOVE_DECK_CARD,
+} from "../actions";
+
+export const decks = ({ state = {}, action }) => {
+  switch (action.type) {
+    case RECEIVE_DECKS:
+      return {
+        ...state,
+        ...action.decks,
+      };
+
+    case ADD_DECK:
+      return {
+        ...state,
+        deck,
+      };
+
+    case ADD_CARD_TO_DECK:
+      return {
+        ...state,
+        ...{
+          ...state[action.deck],
+          questions: [...state[action.deck].questions, action.card],
+        },
+      };
+
+    case REMOVE_DECK:
+      const { [action.title]: removedDeck, ...restDecks } = state;
+
+      return {
+        restDecks,
+      };
+
+    case REMOVE_DECK_CARD:
+      return {
+        ...state,
+        ...{
+          ...state[action.deck],
+          questions: state[action.deck].questions.map(
+            (card) => card !== action.card
+          ),
+        },
+      };
+
+    default:
+      state;
+  }
+};
