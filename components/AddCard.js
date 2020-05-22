cb5c032cd8d566834ff181fe51adbe452895f8e6
jsc <jsanchezcriado@gmail.com>
2020-05-22 23:30:31 +0200

    turned AddCard into a Controlled component

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, TextInput } from "react-native";
 import { TextButton } from "./TextButton";
 
 export const AddCard = (props) => {
-  const { questionValue, answerValue } = props;
+  console.log("AddCard props", props);
+
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
   return (
     <View>
       <TextInput
         style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        onChangeText={(text) => console.log(text)}
-        value={questionValue}
+        onChangeText={(text) => setQuestion(text)}
+        value={question}
         placeholder="Question"
       />
       <TextInput
         style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        onChangeText={(text) => console.log(text)}
-        value={answerValue}
+        onChangeText={(text) => setAnswer(text)}
+        value={answer}
         placeholder="Answer"
       />
-      <TextButton onPress={() => alert("Submit")} style={{ padding: 10 }}>
+      <TextButton
+        onPress={() => alert(`Submit ${question} and ${answer}`)}
+        style={{ padding: 10 }}
+        disabled={question === "" || answer === ""}
+      >
         Submit
       </TextButton>
     </View>
