91dd80bd64e9f58076c25cc7f4355198f7b515fb
jsc <jsanchezcriado@gmail.com>
2020-05-22 14:08:18 +0200

    add some styles to AddDeck

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import { TextButton } from "./TextButton";
 import { connect } from "react-redux";
 import { addDeck } from "../actions";
+import { red, blue } from "../utils/colors";
 
 const AddDeck = (props) => {
   console.log("AddDeck props", props);
@@ -20,21 +21,22 @@ const AddDeck = (props) => {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
-        style={{
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          margin: 16,
-          padding: 4,
-        }}
+        style={styles.input}
         onChangeText={(text) => setDeckName(text)}
         value={deckName}
         placeholder="Deck Name"
       />
 
-      <TextButton onPress={submitDeck} style={{ padding: 10 }}>
+      <TextButton
+        onPress={submitDeck}
+        style={[
+          deckName === "" ? styles.disabled : styles.enabled,
+          styles.button,
+        ]}
+        disabled={deckName === ""}
+      >
         Submit
       </TextButton>
     </View>
@@ -42,3 +44,25 @@ const AddDeck = (props) => {
 };
 
 export const ConnectedAddDeck = connect()(AddDeck);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 4,
+  },
+  button: {
+    margin: 30,
+  },
+  disabled: {
+    backgroundColor: red,
+  },
+  enabled: {
+    backgroundColor: blue,
+  },
+});
